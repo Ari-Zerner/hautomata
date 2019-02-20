99c8cdcd33cd53f30859b8a8dbfc917993613e5e
Ari Zerner <ariel@zerner.com>
2019-02-20 08:29:27 -0500

    Restructured FSA in preparation for NFAs

--- src/Automata/FSA.hs
+++ src/Automata/FSA.hs
@@ -1,9 +1,9 @@
-{-# LANGUAGE RecordWildCards, MultiParamTypeClasses, FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances #-}
 
 module Automata.FSA (
-  FSA,
-  fsa,
-  state,
+  DFA,
+  dfa,
+  currentState,
   acceptingStates,
   transitions,
   accepts
@@ -15,51 +15,51 @@ import qualified Data.Map.Strict as Map
 import Data.Maybe
 import Control.Monad
 
--- |A finite state automaton.
-data FSA state symbol =
-  FSA { current     :: state
-      , accepting   :: Set.Set state
-      , transitionMap :: Map.Map (state, symbol) state
-      }
+-- |The non-state data of a finite state automaton.
+data FSADef state symbol =
+  FSADef
+    (Set.Set state) -- ^accepting states
+    (Map.Map (state, symbol) state) -- ^transitions
   deriving (Show) -- TODO: remove in production
 
--- |Create a new FSA.
-fsa :: (Ord state, Ord symbol)
+-- |A deterministic finite state automaton.
+data DFA state symbol = DFA (FSADef state symbol) state
+  deriving (Show) -- TODO: remove in production
+
+-- |Create a new DFA.
+dfa :: (Ord state, Ord symbol)
     => state -- ^The initial state
     -> [state] -- ^The accepting states
     -> [(state,  [(symbol, state)])] -- ^For each state, the list of transitions from that state
-    -> FSA state symbol
-fsa start accept trans = FSA { current = start
-                             , accepting = Set.fromList accept
-                             , ..
-                             }
+    -> DFA state symbol
+dfa initial accepting transitions = DFA (FSADef (Set.fromList accepting) transitionMap) initial
   where transitionMap = Map.fromList [ ((state, symbol), newState)
-                                | (state, stateTrans) <- trans
-                                , (symbol, newState) <- stateTrans
-                                ]
+                                     | (state, stateTransitions) <- transitions
+                                     , (symbol, newState) <- stateTransitions
+                                     ]
 
--- |Get the current state of a FSA.
-state :: FSA state symbol -> state
-state = current
+-- |Get the current state of a DFA.
+currentState :: DFA state symbol -> state
+currentState (DFA _ current) = current
 
--- |Get the accepting states of a FSA.
-acceptingStates :: FSA state symbol -> [state]
-acceptingStates FSA{..} = Set.toList accepting
+-- |Get the accepting states of a DFA.
+acceptingStates :: DFA state symbol -> [state]
+acceptingStates (DFA (FSADef accepting _) _) = Set.toList accepting
 
--- |Get the transitions of a FSA, organized by source state.
-transitions :: (Ord state, Ord symbol) => FSA state symbol -> [(state,  [(symbol, state)])]
-transitions FSA{..} = Map.toList $ Map.toList <$> Map.foldrWithKey aux Map.empty transitionMap
+-- |Get the transitions of a DFA, organized by source state.
+transitions :: (Ord state, Ord symbol) => DFA state symbol -> [(state,  [(symbol, state)])]
+transitions (DFA (FSADef _ trans) _) = Map.toList $ Map.toList <$> Map.foldrWithKey aux Map.empty trans
   where aux (q1, s) q2 acc = Map.insertWith Map.union q1 (Map.singleton s q2) acc
 
--- |Determine whether a FSA accepts a given list of symbols.
-accepts :: (Ord state, Ord symbol) => FSA state symbol -> [symbol] -> Bool
+-- |Determine whether a DFA accepts a given list of symbols.
+accepts :: (Ord state, Ord symbol) => DFA state symbol -> [symbol] -> Bool
 accepts m input = maybe False (isAccept . decide) $ foldM (flip step) m input
 
-instance (Ord state, Ord symbol) => Steppable symbol (FSA state symbol) where
-  step s FSA{..} = (\current -> FSA{..}) <$> transitionMap Map.!? (current, s)
+instance (Ord state, Ord symbol) => Steppable symbol (DFA state symbol) where
+  step s (DFA (FSADef accepting trans) current) = (DFA $ FSADef accepting trans) <$> trans Map.!? (current, s)
 
-instance (Ord state) => PartialDecider (FSA state symbol) where
+instance (Ord state) => PartialDecider (DFA state symbol) where
   partialDecide = Decided . decide
 
-instance (Ord state) => Decider (FSA state symbol) where
-  decide FSA{..} = if Set.member current accepting then Accept else Reject
+instance (Ord state) => Decider (DFA state symbol) where
+  decide (DFA (FSADef accepting _) current) = if Set.member current accepting then Accept else Reject
--- test/Test/Automata/FSA.hs
+++ test/Test/Automata/FSA.hs
@@ -9,51 +9,51 @@ import Data.Maybe
 import qualified Data.Set as Set
 import Data.List
 
-type FSA' = FSA Integer Char
+type DFA' = DFA Integer Char
 
 prop_initState :: Integer -> Bool
-prop_initState start = state m == start
-  where m = fsa start [] [] :: FSA'
+prop_initState start = currentState m == start
+  where m = dfa start [] [] :: DFA'
 
 prop_stepState :: Integer -> Integer -> Char -> Bool
-prop_stepState q1 q2 c = maybe False ((== q2) . state) (step c m)
-  where m = fsa q1 [] [(q1, [(c, q2)])] :: FSA'
+prop_stepState q1 q2 c = maybe False ((== q2) . currentState) (step c m)
+  where m = dfa q1 [] [(q1, [(c, q2)])] :: DFA'
 
 prop_noStep1 :: Integer -> Char -> Bool
 prop_noStep1 start c = isNothing $ step c m
-  where m = fsa start [] [] :: FSA'
+  where m = dfa start [] [] :: DFA'
 
 prop_noStep2 :: Integer -> Char -> Bool
 prop_noStep2 start c = isNothing $ step c m
-  where m = fsa start [] [(start, [])]
+  where m = dfa start [] [(start, [])]
 
 prop_noStep3 :: Integer -> Char -> Char -> Property
 prop_noStep3 start c1 c2 = c1 /= c2 ==> isNothing $ step c2 m
-  where m = fsa start [] [(start, [(c1, start)])]
+  where m = dfa start [] [(start, [(c1, start)])]
 
 prop_decide :: Integer -> [Integer] -> Bool
 prop_decide start accept = isAccept (decide m) == elem start accept
-  where m = fsa start accept [] :: FSA'
+  where m = dfa start accept [] :: DFA'
 
 prop_total :: Integer -> [Integer] -> [(Integer, [(Char, Integer)])] -> Bool
-prop_total start accept trans = partialDecide (fsa start accept trans) /= Undecided
+prop_total start accept trans = partialDecide (dfa start accept trans) /= Undecided
 
 prop_acceptingStates :: Integer -> [Integer] -> Bool
 prop_acceptingStates start accept = Set.fromList (acceptingStates m) == Set.fromList accept
-  where m = fsa start accept [] :: FSA'
+  where m = dfa start accept [] :: DFA'
 
 prop_transitions :: Integer -> [Integer] -> [(Integer, [(Char, Integer)])] -> Bool
 prop_transitions start accept trans = flatSet (deDup trans) == flatSet trans'
-  where trans'    = transitions $ fsa start accept (deDup trans)
+  where trans'    = transitions $ dfa start accept (deDup trans)
         fstEq a b = fst a == fst b
         deDup     = map (fmap (nubBy fstEq)) . nubBy fstEq
         flatSet t = Set.fromList [(q1, s, q2) | (q1, sAndQ2s) <- t, (s, q2) <- sAndQ2s]
 
 propAccepts :: Bool -> Integer -> [Integer] -> [(Integer, [(Char, Integer)])] -> [Char] -> Property
 propAccepts expected start accept trans sigma =
-  forAll (listOf $ elements sigma) $ \input -> expected == (accepts (fsa start accept trans) input)
+  forAll (listOf $ elements sigma) $ \input -> expected == (accepts (dfa start accept trans) input)
 
 prop_accepts_1 = propAccepts False 0 [] [] "ab"
-prop_accepts_2 = accepts (fsa 0 [0] []) ""
+prop_accepts_2 = accepts (dfa 0 [0] []) ""
 prop_accepts_3 = propAccepts True 0 [0] [(0, [('a', 0)])] "a"
 -- TODO? More properties for `accepts`
